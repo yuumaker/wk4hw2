1642c5f6627a4aa9b7df6240f20dc67872bd69a2
yuumaker <48975518+yuumaker@users.noreply.github.com>
2020-04-13 22:05:15 +0800

    Add files via upload

--- App.js
+++ App.js
@@ -2,12 +2,18 @@ import React from "react";
 import { View } from "react-native";
 import Header from "./src/components/Header";
 import AlbumList from "./src/components/AlbumList";
+import Bottom from "./src/components/Bottom";
+import Drawer from "./src/components/Drawer";
+import Black from "./src/components/Black";
 
 const App = () => {
   return (
     <View>
       <Header />
       <AlbumList />
+      <Bottom />
+      <Black />
+      <Drawer />
     </View>
   );
 };
